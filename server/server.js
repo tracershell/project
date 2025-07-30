b26b4baec85f7033e3455034aaf7808c5dfeec58
tracershell <apple2ne1@me.com>
2025-07-30 12:23:24 -0700

    CD Docker connect ID modify

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3033;
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from server!' });
+  res.json({ message: 'Hello from server! - Modify' });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
