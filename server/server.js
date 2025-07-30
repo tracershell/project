5d2818e18a9481eecabbdf0e9ca980f46030094f
tracershell <apple2ne1@me.com>
2025-07-30 15:08:38 -0700

    Add SSH New key

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3033;
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from server! - Modify -add' });
+  res.json({ message: 'Hello from server! - Modify -add SSH NEW KEY' });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
