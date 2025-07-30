9d53d876ce75c88a1ab326015557eaecd63f8dee
tracershell <apple2ne1@me.com>
2025-07-30 16:10:11 -0700

    public & private key

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3033;
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from server! - Modify -add SSH NEW KEY - Location Changed - ci variable modify' });
+  res.json({ message: 'Hello from server! - Modify -add SSH NEW KEY - Location Changed - ci variable modify - public key & private key' });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
