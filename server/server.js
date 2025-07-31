71e70c761d03a1c15490b9981de288db27b91e74
tracershell <apple2ne1@me.com>
2025-07-31 12:57:29 -0700

    DEPLOY_PAHT Changed

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3033;
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from server! - all most' });
+  res.json({ message: 'Hello from server! - change deploy path' });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
