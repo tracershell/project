0ab4ffbf0ddd170755f91ae736c82d496fb46b64
tracershell <apple2ne1@me.com>
2025-07-31 12:41:23 -0700

    all most

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3033;
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from server! - Modify -add SSH NEW KEY - Location Changed - ci variable modify - public key & private key' });
+  res.json({ message: 'Hello from server! - all most' });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
