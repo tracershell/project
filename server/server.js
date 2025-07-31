404f702fef6c99fdce71b6e2b708b0003a43818c
tracershell <apple2ne1@me.com>
2025-07-31 13:41:23 -0700

    modify docker-compose for image

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3033;
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from server! - change deploy path - ci.yml directory modification' });
+  res.json({ message: 'Hello from server! - modify docker-compose.yml for image' });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
