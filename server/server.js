fd5f82e4706bbb9912f4a89eac7bef747c8f0725
tracershell <apple2ne1@me.com>
2025-08-01 11:57:14 -0700

    P 15

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3033;
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from server! - nginx copy - permission' });
+  res.json({ message: 'Hello from server! - nginx copy - permission 15' });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
